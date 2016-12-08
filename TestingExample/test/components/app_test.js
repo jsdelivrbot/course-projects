05d7fed670cbc9999c5d609e753ae501c2890936
James Buczkowski <james.sullenriot@gmail.com>
2016-12-08 16:21:43 -0500

    Added testing example

---
+++ TestingExample/test/components/app_test.js
@@ -0,0 +1,17 @@
+// these come from the test_helper.js file
+import { renderComponent, expect } from '../test_helper';
+import App from '../../src/components/app';
+
+
+// Use "describe" to group together similar tests
+// Testing Component App
+describe('App', () => {
+  // Use "it" to test a single attribute of a target
+  it('shows the correct text', () => {
+    // create an instance of App
+    const component = renderComponent(App);
+    
+    // Use "expect" to make an 'assertion' about a target
+    expect(component).to.contain('React simple starter');
+  });
+});
