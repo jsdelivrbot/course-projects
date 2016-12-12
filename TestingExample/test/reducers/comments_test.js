949ec83242dfe653bd58ec3d5e68a018ebefc0b7
James Buczkowski <james.sullenriot@gmail.com>
2016-12-11 20:31:04 -0500

    More comment box testing

--- TestingExample/test/reducers/comments_test.js
+++ TestingExample/test/reducers/comments_test.js
@@ -5,8 +5,8 @@ import { SAVE_COMMENT } from '../../src/actions/types';
 describe('Comments Reducer', () => {
   it('handles action with unknown type', () => {
     //expect whatever the reducer returns to be an array
-    expect(commentReducer()).to.be.instanceof(Array);
-    expect(commentReducer()).to.eql([]);
+  //  expect(commentReducer()).to.be.instanceof(Array);
+    expect(commentReducer(undefined, {})).to.eql([]);
   });
 
   it('handles action of type SAVE_COMMENT', () => {
