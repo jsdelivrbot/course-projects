6773e27ce83ed1ef6401b594be6a88da96a603ee
James Buczkowski <james.sullenriot@gmail.com>
2016-12-13 21:03:02 -0500

    Final higher order component example

---
+++ HigherOrderComponents/src/components/require_authentication.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default function(ComposedComponent) {
+  class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+
+    // whenever this component is about to render
+    // kick the users back to the home route
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    // when the component is about to get a new set of props or be rerendered
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    render() {
+      // get access to context
+      console.log(this.context);
+
+      //console.log('Rendering', ComposedComponent);
+      //console.log(this.props.authenticated);
+      return <ComposedComponent {...this.props} />
+    }
+  }
+
+  function mapStateToProps(state) {
+    return { authenticated: state.authenticated };
+  }
+
+  // this is technically nesting a higher order component
+  return connect(mapStateToProps)(Authentication);
+}
