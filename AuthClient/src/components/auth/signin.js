ff8a9e1b16ff921de8b488b1562540bba9c47861
James Buczkowski <james.sullenriot@gmail.com>
2016-12-16 22:35:21 -0500

    Added some header logic

--- AuthClient/src/components/auth/signin.js
+++ AuthClient/src/components/auth/signin.js
@@ -34,7 +34,7 @@ class Signin extends Component {
         </fieldset>
         <fieldset className="form-group">
           <label>Password:</label>
-          <input {...password} className="form-control" />
+          <input {...password} type="password" className="form-control" />
         </fieldset>
 
         {this.renderAlert()}
