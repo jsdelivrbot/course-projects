e8a83f2d3971c58d89819e76f14b1bb429ae8aeb
James Buczkowski <james.sullenriot@gmail.com>
2016-12-16 23:31:58 -0500

    Scaffolding of signup form

---
+++ AuthClient/src/components/auth/signup.js
@@ -0,0 +1,39 @@
+// collect email, password, and password confirmation
+// from the user then once we have the info
+// send to back end and create an account
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import * as actions from '../../actions';
+
+class Signup extends Component {
+  render() {
+    const { handleSubmit, fields: { email, password, passwordConfirm}} = this.props;
+
+    return (
+      <form>
+        <fieldset className="form-group">
+          <label>Email:</label>
+          <input className="form-control" {...email} />
+        </fieldset>
+
+        <fieldset className="form-group">
+          <label>Password:</label>
+          <input type="password" className="form-control" {...password} />
+        </fieldset>
+
+        <fieldset className="form-group">
+          <label>Confirm Password:</label>
+          <input type="password" className="form-control" {...passwordConfirm} />
+        </fieldset>
+
+        <button action="submit" className="btn btn-primary">Signup </button>
+
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'signup',
+  fields: ['email', 'password', 'passwordConfirm']
+})(Signup);
