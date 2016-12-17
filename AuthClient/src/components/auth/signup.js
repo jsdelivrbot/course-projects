fda9ec4c5e4281a257f585f4c55e2212f71ca680
James Buczkowski <james.sullenriot@gmail.com>
2016-12-17 00:01:00 -0500

    Added more error validation for remaining parts of signup form

--- AuthClient/src/components/auth/signup.js
+++ AuthClient/src/components/auth/signup.js
@@ -14,16 +14,19 @@ class Signup extends Component {
         <fieldset className="form-group">
           <label>Email:</label>
           <input className="form-control" {...email} />
+          {email.touched && email.error && <div className="error">{email.error}</div>}
         </fieldset>
 
         <fieldset className="form-group">
           <label>Password:</label>
           <input type="password" className="form-control" {...password} />
+          {password.touched && password.error && <div className="error">{password.error}</div>}
         </fieldset>
 
         <fieldset className="form-group">
           <label>Confirm Password:</label>
           <input type="password" className="form-control" {...passwordConfirm} />
+          {passwordConfirm.touched && passwordConfirm.error && <div className="error">{passwordConfirm.error}</div>}
         </fieldset>
 
         <button action="submit" className="btn btn-primary">Signup </button>
@@ -33,7 +36,32 @@ class Signup extends Component {
   }
 }
 
+function validate(formProps) {
+  const errors = {};
+  // refactor to remove duplicate code
+  // foreach over formProps
+  if (!formProps.email) {
+    errors.email = "Please enter an email";
+  }
+
+  if (!formProps.password) {
+    errors.password = "Please enter a password";
+  }
+
+  if (!formProps.passwordConfirm) {
+    errors.passwordConfirm = "Please enter a password confirmation";
+  }
+
+  if (formProps.password !== formProps.passwordConfirm) {
+    errors.password = 'Passwords Must Match';
+  }
+
+  return errors; // by default always return this even if empty
+}
+
+
 export default reduxForm({
   form: 'signup',
-  fields: ['email', 'password', 'passwordConfirm']
+  fields: ['email', 'password', 'passwordConfirm'],
+  validate
 })(Signup);
