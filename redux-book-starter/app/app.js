9a5b4c43349ed6880b4affd85e3052d41d873112
James Buczkowski <james.sullenriot@gmail.com>
2017-04-08 15:01:06 -0400

    wip redux book

--- redux-book-starter/app/app.js
+++ redux-book-starter/app/app.js
@@ -2,16 +2,41 @@ import 'normalize.css/normalize.css';
 import 'assets/stylesheets/main.css';
 import { createStore } from 'redux';
 
+
+// WRONG WAY TO CREATE A REDUCER
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case 'ADD_RECIPE':
+//       state.recipes.push({ name: action.name });
+//   }
+//   return state;
+// };
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'INC':
-      return state + 1;
+    case 'ADD_RECIPE':
+      return Object.assign({}, state, {
+        recipes: state.recipes.concat({
+          name: action.name
+        });
+      })
   }
-
   return state;
-};
+}
 
-const initialState = 0;
+const initialState = {
+   recipes: [
+     {
+       name: 'Omelette'
+     }
+   ],
+   ingredients: [
+     {
+       recipe: 'Omelette',
+       name: 'Egg',
+       quantity: 2
+     }
+   ]
+ };
 
 const store = createStore(reducer, initialState);
 
@@ -20,3 +45,5 @@ store.subscribe(() => document.getElementById('counter').innerText = store.getSt
 setInterval(() => store.dispatch({ type: 'INC' }), 500);
 
 console.log("Redux started");
+
+window.store = store;
