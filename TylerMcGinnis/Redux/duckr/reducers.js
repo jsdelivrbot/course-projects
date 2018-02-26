4de4dfcf3dbf16b79e54b09fec077ee84845b128
James Buczkowski <james.sullenriot@gmail.com>
2018-02-25 21:51:02 -0500

    video part 2 - transforming state with reducers. Pick up at 7:30. Adding all reducers for each action

--- TylerMcGinnis/Redux/duckr/reducers.js
+++ TylerMcGinnis/Redux/duckr/reducers.js
@@ -72,3 +72,139 @@ function users (state = initialState, action) {
         return state
   }
 }
+
+
+// Ducks
+const initialState = {
+  isFetching; true,
+  error: ''
+}
+
+function ducks (state, action) {
+  switch (action.type) {
+    case FETCHING_DUCK:
+      return {
+        ...state,
+        isFetching: true,
+      }
+    case ADD_DUCK:
+    case FETCHING_DUCK_SUCCESS:
+      return {
+        ...state,
+        error: '',
+        isFetching: false,
+        [action.duck.duckId]: action.duck,
+      }
+    case FETCHING_DUCK_ERROR:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error,
+      }
+    case REMOVE_FETCHING:
+      return {
+        ...state,
+        isFetching: false,
+        error: '',
+      }
+    case ADD_MULTIPLE_DUCKS:
+      return {
+        ...state,
+        ...action.ducks
+      }
+    default:
+      return state
+  }
+}
+
+
+// Feed
+const initialState ={
+    isFetching: false,
+    newDucksAvailable: false,
+    newDucksToAdd: [],
+    error: '',
+    duckIds: [],
+}
+
+function feed (state, action) {
+  switch (action.type {
+    case SETTING_FEED_LISTENER:
+      return {
+        ...state,
+        isFetching: true,
+      }
+    case SETTING_FEED_LISTENER_ERROR:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error,
+      }
+    case SETTING_FEED_LISTNER_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        error: '',.
+        duckIds: action.duckIds,
+        newDucksAvailable: false,
+      }
+    case ADD_NEW_DUCK_ID_TO_FEED:
+      return {
+        ...state,
+        newDucksToAdd: [action.duckId, ... state.newDucksToAdd]
+      }
+    case RESET_NEW_DUCKS_AVAILABLE:
+      return {
+        ...state,
+        duckIds: [...state.newDucksToAdd, ...state.duckIds],
+        newDucksToAdd: [],
+        newDucksAvailable: false,
+      }
+    default:
+      return state
+  })
+}
+
+
+
+//Listeners
+export default function listeners (state = {}, action) {
+  switch (action.type) {
+    case ADD_LISTENER :
+      return {
+        ...state,
+        [action.listenerId]: true,
+      }
+    default :
+      return state
+  }
+}
+
+
+//Modal
+const initialState = {
+  duckText: '',
+  isOpen: false,
+}
+
+export default function modal (state = initialState, action) {
+  switch (action.type) {
+    case OPEN_MODAL :
+      return {
+        ...state,
+        isOpen: true,
+      }
+    case CLOSE_MODAL :
+      return {
+        duckText: '',
+        isOpen: false,
+      }
+    case UPDATE_DUCK_TEXT :
+      return {
+        ...state,
+        duckText: action.newDuckText,
+      }
+    default :
+      return state
+  }
+}
